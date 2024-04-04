0b0a9cc3e87d0f330acfa2489d414be9c8aa99a7
Your Name <you@example.com>
2024-04-04 16:32:26 +0800

    91%

--- Mariveles Grab/src/App.jsx
+++ Mariveles Grab/src/App.jsx
@@ -4,7 +4,7 @@ import MiddleMenu from "./Middle/MiddleMenu";
 import { MiddleDetails } from "./Middle/Middle.js";
 
 function App() {
-  const [selectedMiddleMenu, setSelectedMiddleMenu] = useState(null);
+  const [selectedMiddleMenu, setSelectedMiddleMenu] = useState("Consumer");
 
   function handleClick(selectedButton) {
     setSelectedMiddleMenu(selectedButton);
@@ -65,7 +65,39 @@ function App() {
             </div>
           ))}
         </div>
+
         <hr className="custom-hr" />
+
+        <div>
+          <h2>Newsroom</h2>
+
+          <section id="">
+            <a href=""><img src="src\Middle\news-image1.png" alt="driver" /></a>
+            <a href=""><img src="src\Middle\enterprise-ads.jpg" alt="mobile" /></a>
+            <a href=""><img src="src\Middle\news-image3.png" alt="grab" /></a>
+            <a href=""><img src="src\Middle\news-image4.png" alt="delivery" /></a>
+          </section>
+          <div id="date">
+            <label>01/03/24</label>
+            <label>25/01/24</label>
+            <label>17/01/24</label>
+            <label>12/01/24</label>
+          </div>
+          <div id="">
+            <p>A Superapp as Wingman: Grab Helps Thousands of Filipinos with Valentine’s Day Plans</p>
+            <p>Grab Outlines New Affordable Solutions So YOU Can Enjoy More for Less</p>
+            <p>Grab Launches GrabCar in CDO, Strengthens Economic Empowerment Commitment in the City</p>
+            <p>Filipinos increase adoption of online-to-offline dining solutions: Grab 2023 Report</p>
+          </div>
+
+          <div id="">
+            <p>Read More</p>
+            <p>Read More</p>
+            <p>Read More</p>
+            <p>Read More</p>
+          </div>
+        </div>
+
       </section>
     </div>
   );
